75523a715c11e21d3bc9abb99ed0c32e0baa0849
Odiong <enalsy22@gmail.com>
2026-02-06 15:21:16 +0100

    Added Emergency Withdrawal Tests

--- contractz/tests/vitest.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest/globals" />
-/// <reference types="@stacks/clarinet-sdk/vitest-helpers" />
-
-import { Simnet } from "@stacks/clarinet-sdk";
-
-declare global {
-  const simnet: Simnet;
-}
-
